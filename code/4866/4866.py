54ba5afd4be6b29b5bf89ef20f4c5815e3cc0622
unknown <yuueuni@gmail.com>
2020-02-06 09:33:42 +0900

    algorithm

--- code/4866/4866.py
+++ code/4866/4866.py
@@ -10,22 +10,4 @@ for tc in range(1, T+1):
     s = []
     balance = True
     idx = 0
-
-    while idx < len(test) and balance:
-        temp = test[idx]
-
-        if temp == '(' or temp == '{':
-            s.append(temp)
-        else:
-            if s:
-                balance = False
-            else:
-                s.pop()
-
-        idx += 1
-
-    if balance and s:
-        result = True
-    else:
-        result = False
-    print(result)
+    print()
--- code/4869/4869.py
+++ code/4869/4869.py
@@ -1,12 +1,15 @@
 import sys
 
-sys.stdin = open('.\input.txt')
+sys.stdin = open('input.txt')
 
 T = int(input())
 
 for tc in range(1, T+1):
     n = int(input())
-    d = n//10
-    result = 0
+    d = n // 20
+    if d%2:
+        result = (2**(d-1))*4+1
+    else:
+        result = (2**(d-1))*4
 
     print(f'#{tc} {result}')
