d7eed5a0a1822a79817b740db0a583020f08fcc7
unknown <yuueuni@gmail.com>
2020-02-06 22:21:54 +0900

    SWEA_D3

--- SWEA_D3/4615/4615_othello.py
+++ SWEA_D3/4615/4615_othello.py
@@ -9,27 +9,39 @@ def othello(n, matrix, a, b, color):
     # 위부터 시계방향순
     dy = [-1, -1, 0, 1, 1, 1, 0, -1] # b = j
     dx = [0, 1, 1, 1, 0, -1, -1, -1] # a = i
+    
+    # 8방향 확인
     for i in range(8):
+
+        # 변경되는 인덱스 리스트
         chg_idx = []
+        # 변경 기준 인덱스 /= 같은 컬러인 기준 인덱스
         idx = []
+
         ta = a + dx[i]
         tb = b + dy[i]
+        # 8방향 변경 ---1
         temp = matrix[tb][ta]
+        # 1과 같은 방향으로 인덱스 변경해가며 같은색이나 0(빈칸) 확인
         while temp != 0:
+            # 다른 컬러: 변경해야 할 인덱스에 추가
             if temp != c:
                 chg_idx.append([ta, tb])
+            # 같은 컬러 나와야 변경가능하므로 변경기준 인덱스 확인
             elif temp == c:
                 idx.append([ta, tb])
                 break
+            # 1과 같은방향
             ta = ta + dx[i]
             tb = tb + dy[i]
             temp = matrix[tb][ta]
+        # 같은컬러 없으면 변경 X
+        # - 유무 확인
         if idx:
+            # 변경할 인덱스 리스트로 색 변경
             for j in chg_idx:
                 matrix[j[1]][j[0]] = c
     return matrix
-    
-    
 
 
 for tc in range(1, T+1):
@@ -46,6 +58,7 @@ for tc in range(1, T+1):
         i, j, c = map(int, input().split())
         board[j][i] = c
         board = othello(n, board, i, j, c)
+    # 각 라인별 색 카운트
     black = sum(bla.count(1) for bla in board)
     white = sum(wh.count(2) for wh in board)
     print(f'#{tc}',black, white)
