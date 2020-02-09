a71c5268a9dee2fabb35241863978272432b67e3
unknown <yuueuni@gmail.com>
2020-02-09 21:29:22 +0900

    SWEA_D4

--- SWEA_D4/1211/re_1211.py
+++ SWEA_D4/1211/re_1211.py
@@ -8,9 +8,11 @@ def path(boardi, xi, yi):
     while yi < 99:
         # 왼쪽
         if boardi[yi][xi-1]:
+            # 왼쪽 길 있으면 쭉 진행
             while boardi[yi][xi-1]:
                 xi -= 1
                 mv += 1
+            # 길 막혔으니까 밑으로 하나 내려감
             yi += 1
             mv += 1
         # 오른쪽
@@ -20,6 +22,7 @@ def path(boardi, xi, yi):
                 mv += 1
             yi += 1
             mv += 1
+        # 밑으로만 쭉쭉 내려감
         else:
             yi += 1
             mv += 1
@@ -29,15 +32,17 @@ def path(boardi, xi, yi):
 
 for _ in range(10):
     tc = int(input())
+    # x 처음 끝 인덱스 범위 방지 위해 양쪽 끝에 의미없는 0 추가
     board = [ [0] + list(map(int, input().split())) + [0] for _ in range(100)]
     start_index = list(x for x in range(102) if board[0][x])
     
     result = {}
     for start in start_index:
+        # key : 출발지, value:이동거리
         result[start] = path(board, start, 0)
     
+    # 제일 짧은 거리
     mi = min(result.values())
+    # 제일 짧은 거리인 출발지 리스트
     mx_idx = list(x-1 for x in result if result[x] == mi)
-    
-    #print('no', sorted(result.items()))
     print(f'#{tc}', max(mx_idx))
--- SWEA_D4/1211/test2.py
+++ SWEA_D4/1211/test2.py
@@ -1,59 +1,53 @@
 import sys
 
-sys.stdin = open('c:\\Users\\multicampus\\Desktop\\vscode\\algorithm\\SWEA_D4\\1211\\input.txt')
-
+sys.stdin = open('C:\\Users\\jin89\\Desktop\\algorithm\\SWEA_D4\\1211\\input.txt')
 
 
 # 사다리 타기
 # 사다리 맵, 시작위치를 받아서 카운트를 반환
-def go(ladder, x, y):
+def go(ladder, pos):
     #    좌 우 하
     dx = [-1, 1, 0]
     dy = [0, 0, 1]
-    length = 100
-
-    cnt = 0
-
+    x = pos[1] 
+    y = pos[0]
+    length = len(ladder)
+    cnt = 1
     while y < length: # 끝까지 가지 않았다면 반복
         for i in range(2):
-            x = x + dx[i]
-            y = y + dy[i]
+            tmp_x = x + dx[i]
+            tmp_y = y + dy[i]
             # 좌우에 길이 있다을 때
-            if 0 <= x < length and 0 <= y < length and ladder[x][y] == 1:
-                #print('방향전환 ^^')
+            if 0 <= tmp_x < length and 0 <= tmp_y < length and ladder[tmp_y][tmp_x] == 1:
                 # 벽을 만나거나 0을 만나지 않는다면 계속 가라.
                 while 0 <= x < length and 0 <= y < length and ladder[y][x] ==1:
                     x += dx[i]
-                    y += dy[i]
                     cnt +=1
-                    #print(y, x, cnt)
                 # 만약 다음 자리가 벽이나 0이 있다면 dx[i], dy[i]만큼 돌아간 후, 아래로 이동해라
-                #print('▲▲▲▲▲▲▲▲▲▲ ------  deleted')
                 x += dx[2] - dx[i]
                 y += dy[2] - dy[i]
-                #print(y, x, cnt)
-        # 좌우에 길이 없다면 아래로 가라
+            # 좌우에 길이 없다면 아래로 가라
         else:
-            x += dx[2]
-            y += dy[2]
+            y += 1
             cnt += 1
-            #print(y, x, cnt)
     return cnt
 
 
-
-for tc in range(10):
+for pas in range(10):
     T = int(input())
     ladder = [list(map(int, input().split())) for _ in range(100)]                
     start_pos = [idx for idx, num in enumerate(ladder[0]) if num == 1] # 시작하는 x좌표의 위치
-    #m_cnt = 10000
+    m_cnt = 10000
     result_list = {}
     for s in start_pos:
-        cnt = go(ladder, s, 0)
+        pos = [0, s]
+        cnt = go(ladder, pos)
         result_list[s] = cnt
+        if cnt < m_cnt:
+            m_cnt = cnt
+            result = s
     
-
+    print(result_list)
     mi = min(result_list.values())
-    mx_idx = list(x-1 for x in result_list if result_list[x] == mi)
+    mx_idx = list(x for x in result_list if result_list[x] == mi)
     print(f'#{T}', max(mx_idx))
-    # print(result, result_list)
