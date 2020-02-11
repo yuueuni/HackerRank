f39acf05da0375b8daeef5052958a2e312a1fee4
yoon <yuueuni@gmail.com>
2020-02-11 17:53:25 +0900

    SWEA_D4

--- SWEA_D4/1258/1258.py
+++ SWEA_D4/1258/1258.py
@@ -1,33 +1,29 @@
 import sys
 
-sys.stdin = open('c:\\Users\\jin89\\Desktop\\algorithm\\SWEA_D4\\1258\\input.txt')
+sys.stdin = open('input.txt')
 
 T = int(input())
 
 def find(datai, ni):
     idx = []
-    i = j = 0
-    while i < ni:
-        base = datai[i][j]
-        if base:
-            ti = tj = 1
-            while datai[i][j+tj]:
-                datai[i][j+tj] = 0
-                tj += 1
-            while datai[i+ti][j]:
-                datai[i+ti][j] = 0
-                ti += 1
-            datai[i][j] = 0
-            idx.append([ti, tj])
-            j += tj
-            if j> ni:
-                i += ti+1
-                j = 0
-        else:
-            j += 1
-            if j > ni:
-                j = 0
-                i += 1
+    for i in range(ni):
+        for j in range(ni):
+            cnt_i = cnt_j = 1
+            if datai[i][j]:
+                t = 0
+                while 1:
+                    if datai[i+t][j]:
+                        cnt_i = t + 1
+                        for k in range(ni-j):
+                            if datai[i+t][j+k]:
+                                cnt_j = k + 1
+                                datai[i+t][j+k] = 0
+                            else:
+                                break
+                        t += 1
+                    else:
+                        break
+                idx.append([cnt_i, cnt_j])
     return idx
 
 
@@ -37,11 +33,14 @@ for tc in range(1, T+1):
     for _ in range(n):
         data.append(list(map(int, input().split()))+[0])
     result = find(data, n)
+    
     s_result = []
     for r in result:
         temp = r[0]*r[1]
         s_result.append([temp, r[0], r[1]])
     s_result.sort()
+    
+    # 출력
     print(f'#{tc}', len(result), end =' ')
     for sr in s_result:
         stemp = sr[1:]
