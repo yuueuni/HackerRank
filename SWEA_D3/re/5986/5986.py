dac749b8ddf0d44483f9a4a1c595d70fc8c6ddb7
yoon <yuueuni@gmail.com>
2020-04-07 19:45:47 +0900

    Resolve

--- SWEA_D3/re/5986/5986.py
+++ SWEA_D3/re/5986/5986.py
@@ -4,27 +4,26 @@ sys.stdin = open('input.txt')
 
 T = int(input())
 
-def primeNumber(number):
-    printN = []
-    for i in range(2, number+1):
-        check = True
-        for j in range(2, i):
-            if i % j == 0:
-                check = False
-                break
-        if check:
-            printN.append(i)
-    return printN
+primeNumber = []
+for i in range(2, 1000):
+    check = True
+    for j in range(2, i):
+        if i % j == 0:
+            check = False
+            break
+    if check:
+        primeNumber.append(i)
 
 
 def check(number):
-    pn = primeNumber(number)
-    temp = []
-    return 1
+    global primeNumber, cnt
     
+
+    return 1
 
 
 for tc in range(1, T+1):
     n = int(input())
-    result = primeNumber(n)
-    print(f'{tc} {result}')
+    result = check(n)
+    cnt = 0
+    print(f'#{tc} {cnt}')
