80e6ac7c3fbfa17b97423ab49b40fb865725937b
yuueuni <yuueuni@gmail.com>
2020-08-03 22:26:47 +0900

    Add calcu solve

---
+++ SWEA_D3/9700/9700.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+t = int(input())
+
+for tc in range(1, t+1):
+    p, q = map(float, input().split())
+    s1 = (1-p)*q
+    s2 = p*(1-q)*q
+    if s1 < s2:
+        result = 'YES'
+    else:
+        result = 'NO'
+    print(f'#{tc} {result}')
