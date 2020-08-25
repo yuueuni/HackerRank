e5f1776f1613f308ac7189585f19a38f36e17174
yuueuni <yuueuni@gmail.com>
2020-08-25 23:34:58 +0900

    Add collection counter problem

---
+++ programmers/hash/marathon.py
@@ -0,0 +1,23 @@
+# 효율성 fail
+
+def solution(participant, completion):
+    answer = []
+    for person in participant:
+        if person not in answer:
+            completion_person = completion.count(person)
+            participant_person = participant.count(person)
+            count_person = participant_person - completion_person
+            for _ in range(count_person):
+                answer.append(person)
+    return ','.join(answer)
+
+# -------------------------------------------------------------------
+
+# Success
+import collections
+
+def solution2(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
+
+# -------------------------------------------------------------------
