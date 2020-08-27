ab1bff98a3d5aa7307f5b401f721f20dd6c14cb9
yuueuni <yuueuni@gmail.com>
2020-08-28 00:46:04 +0900

    Modify zip, lambda map sort solution

--- programmers/hash/genresPlays.py
+++ programmers/hash/genresPlays.py
@@ -1,10 +1,28 @@
+
+# - Success
+
 def solution(genres, plays):
     answer = []
     gpDict = {genre:[] for genre in set(genres)}
     for gp in zip(genres, plays, range(len(genres))):
         gpDict[gp[0]].append([gp[1], gp[2]])
     gpSort = sorted(list(gpDict.keys()), key=lambda x: sum(map(lambda y: y[0], gpDict[x])), reverse = True)
-    print(gpDict)
-    print(gpSort)
-    answer = list(map(lambda x: x[1], gpDict[s] for s in gpSort))
+    for g in gpSort:
+        gpSortP = [gp[1] for gp in sorted(gpDict[g], key=lambda x: (x[0], -x[1]), reverse = True)]
+        answer += gpSortP[:2]
+    return answer
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+# - Other Soultion
+
+def solution2(genres, plays):
+    answer = []
+    d = {e:[] for e in set(genres)}
+    for e in zip(genres, plays, range(len(plays))):
+        d[e[0]].append([e[1] , e[2]])
+    genreSort =sorted(list(d.keys()), key= lambda x: sum( map(lambda y: y[0],d[x])), reverse = True)
+    for g in genreSort:
+        temp = [e[1] for e in sorted(d[g],key= lambda x: (x[0], -x[1]), reverse = True)]
+        answer += temp[:min(len(temp),2)]
     return answer
